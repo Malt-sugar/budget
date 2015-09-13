9e559cabc833549215c7ede7d15b9c523d315fdc
Kamrul Hasan <saj696@gmail.com>
2015-09-13 13:38:09 +0600

    notification

--- application/helpers/notification_helper.php
+++ application/helpers/notification_helper.php
@@ -94,4 +94,93 @@ class Notification_helper
         return $results;
     }
 
+    public static function get_notification_text($notification)
+    {
+        $CI = & get_instance();
+        $user = User_helper::get_user();
+        $user_division = $user->division_id;
+        $user_zone = $user->zone_id;
+        $user_territory = $user->territory_id;
+        $user_group = $user->budget_group;
+        $return_array = array();
+
+        $id = $notification['id'];
+        $year = $notification['year'];
+        $crop_id = $notification['crop_id'];
+        $type_id = $notification['type_id'];
+        $variety_id = $notification['variety_id'];
+        $sending_division = $notification['sending_division'];
+        $sending_zone = $notification['sending_zone'];
+        $sending_territory = $notification['sending_territory'];
+        $receiving_division = $notification['receiving_division'];
+        $receiving_zone = $notification['receiving_zone'];
+        $receiving_territory = $notification['receiving_territory'];
+        $direction = $notification['direction'];
+        $is_read = $notification['is_read'];
+
+        if($direction == $CI->config->item('direction_up'))
+        {
+            if($user_group == $CI->config->item('user_group_marketing'))
+            {
+                if(isset($sending_division) && !isset($sending_zone) && !isset($sending_territory))
+                {
+                    $return_array['text'] = Notification_helper::get_division_name($sending_division).' Sales target Set!';
+                    $return_array['link'] = '';
+                }
+            }
+            elseif($user_group == $CI->config->item('user_group_division'))
+            {
+                if(isset($sending_division) && isset($sending_zone) && !isset($sending_territory) && $receiving_division == $user_division)
+                {
+                    $return_array['text'] = Notification_helper::get_zone_name($sending_zone).' Sales target Set!';
+                    $return_array['link'] = '';
+                }
+            }
+            elseif($user_group == $CI->config->item('user_group_zone'))
+            {
+                if(isset($sending_division) && isset($sending_zone) && isset($sending_territory) && $receiving_zone == $user_zone)
+                {
+                    $return_array['text'] = Notification_helper::get_territory_name($sending_territory).' Sales target Set!';
+                    $return_array['link'] = base_url().'zi_sales_target/index/add';
+                }
+            }
+        }
+        elseif($direction == $CI->config->item('direction_down'))
+        {
+
+        }
+
+        return $return_array;
+    }
+
+    public static function get_division_name($division_id)
+    {
+        $CI = & get_instance();
+        $CI->db->from('ait_division_info adi');
+        $CI->db->select('adi.division_name');
+        $CI->db->where('adi.division_id', $division_id);
+        $result = $CI->db->get()->row_array();
+        return $result['division_name'];
+    }
+
+    public static function get_zone_name($zone_id)
+    {
+        $CI = & get_instance();
+        $CI->db->from('ait_zone_info azi');
+        $CI->db->select('azi.zone_name');
+        $CI->db->where('adi.zone_id', $zone_id);
+        $result = $CI->db->get()->row_array();
+        return $result['zone_name'];
+    }
+
+    public static function get_territory_name($territory_id)
+    {
+        $CI = & get_instance();
+        $CI->db->from('ait_territory_info ati');
+        $CI->db->select('ati.territory_name');
+        $CI->db->where('ati.territory_id', $territory_id);
+        $result = $CI->db->get()->row_array();
+        return $result['territory_name'];
+    }
+
 }
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,14 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ?>
 
+<div class="row">
+    <div class="pull-right" style="margin-right: 10px;">
+        <span>
+            <img style="width: 30px; height: 20px; margin-top: 10px;" src="<?php echo base_url().'images/envelope.png'?>" />
+            <span>
+                <label style="border-radius: 10px !important; margin-left: -12px;" data-toggle="modal" data-target="#myModal" class="label label-danger">
+                    <?php echo Notification_helper::get_all_notifications();?>
+                </label>
+            </span>
+        </span>
+    </div>
+</div>
+
 <div class="row widget">
     <div class="widget-header">
         <div class="title">
             Dashboard Menu
-            <span style="margin-left: 1050px;">
-                <img style="width: 40px; height: 25px;" src="<?php echo base_url().'images/envelope.png'?>" />
-                <label data-toggle="modal" data-target="#myModal" class="label label-danger"><?php echo Notification_helper::get_all_notifications();?></label>
-            </span>
         </div>
         <div class="clearfix"></div>
     </div>
@@ -50,23 +59,24 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Notification Panel</h4>
             </div>
-            <div class="modal-body">
-                <p>List</p>
 
+            <div class="modal-body">
                 <table class="table table-bordered">
                     <?php
                     $notification_detail = Notification_helper::get_all_notification_detail();
                     foreach($notification_detail as $notification)
                     {
+                        $return_array = Notification_helper::get_notification_text($notification);
                     ?>
                         <tr>
-                            <td><??></td>
+                            <td>
+                                <a class="external" href="<?php echo $return_array['link'];?>" target="_self"><?php echo $return_array['text'];?></a>
+                            </td>
                         </tr>
                     <?php
                     }
                     ?>
                 </table>
-
             </div>
 
             <div class="modal-footer">
