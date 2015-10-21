eea2ed34516dac1dec3190b28744f5dd780787ba
Kamrul Hasan <saj696@gmail.com>
2015-10-21 13:06:10 +0600

    pricing_management ok

--- application/helpers/pricing_helper.php
+++ application/helpers/pricing_helper.php
@@ -42,6 +42,64 @@ class Pricing_helper
         return $data;
     }
 
+    public static function get_pricing_management_info($year, $variety)
+    {
+        $CI = & get_instance();
+        $data = array();
+
+        $CI->db->from('budget_sales_pricing bsp');
+        $CI->db->select('bsp.mrp');
+        $CI->db->where('bsp.year', $year);
+        $CI->db->where('bsp.variety_id', $variety);
+        $CI->db->where('bsp.pricing_type', $CI->config->item('pricing_type_automated'));
+        $result = $CI->db->get()->row_array();
+        $data['automated_mrp'] = isset($result['mrp'])?$result['mrp']:0;
+
+        $last_year = Pricing_helper::get_last_year($year);
+
+        $CI->db->from('budget_sales_pricing bsp');
+        $CI->db->select('bsp.mrp');
+        $CI->db->where('bsp.year', $last_year);
+        $CI->db->where('bsp.variety_id', $variety);
+        $CI->db->where('bsp.pricing_type', $CI->config->item('pricing_type_initial'));
+        $result = $CI->db->get()->row_array();
+        $data['last_year_mrp'] = isset($result['mrp'])?$result['mrp']:0;
+
+        $CI->db->from('budget_indirect_cost_setup bics');
+        $CI->db->select('bics.*');
+        $CI->db->where('bics.year', $year);
+        $result = $CI->db->get()->row_array();
+        $data['ho_and_gen_exp'] = $result['ho_and_gen_exp'];
+        $data['marketing'] = isset($result['marketing'])?$result['marketing']:0;
+        $data['finance_cost'] = isset($result['finance_cost'])?$result['finance_cost']:0;
+        $data['target_profit'] = isset($result['target_profit'])?$result['target_profit']:0;
+        $data['sales_commission'] = isset($result['sales_commission'])?$result['sales_commission']:0;
+
+        // Principal Quantity
+        $CI->db->from('budget_sales_target bst');
+        $CI->db->select('bst.principal_quantity');
+        $CI->db->where('bst.variety_id', $variety);
+        $CI->db->where('bst.year', $year);
+        $CI->db->where('length(bst.customer_id)<2');
+        $CI->db->where('length(bst.territory_id)<2');
+        $CI->db->where('length(bst.zone_id)<2');
+        $CI->db->where('length(bst.division_id)<2');
+        $CI->db->where('bst.status', $CI->config->item('status_active'));
+        $sales_result = $CI->db->get()->row_array();
+        $data['principal_quantity'] = isset($sales_result['principal_quantity'])?$sales_result['principal_quantity']:0;
+
+        // Budgeted PI or COGS
+        $CI->db->from('budget_purchase_quantity bpq');
+        $CI->db->select('bpq.pi_value');
+        $CI->db->where('bpq.variety_id', $variety);
+        $CI->db->where('bpq.year', $year);
+        $CI->db->where('bpq.status', $CI->config->item('status_active'));
+        $budget_purchase_result = $CI->db->get()->row_array();
+        $data['pi_value'] = isset($budget_purchase_result['pi_value'])?$budget_purchase_result['pi_value']:0;
+
+        return $data;
+    }
+
     public static function get_pricing_automated_existing_info($year, $variety)
     {
         $CI = & get_instance();
@@ -107,6 +165,25 @@ class Pricing_helper
         $data['mrp_by_mgt'] = isset($result['mrp'])?$result['mrp']:0;
 
         return $data;
+    }
+
+    public static function get_last_year($year)
+    {
+        $CI = & get_instance();
+        $CI->db->from('ait_year');
+        $CI->db->select('year_name');
+        $CI->db->where('year_id', $year);
+        $result = $CI->db->get()->row_array();
+
+        $this_year = $result['year_name'];
+        $last_year = $this_year - 1;
+
+        $CI->db->from('ait_year');
+        $CI->db->select('year_id');
+        $CI->db->where('year_name', $last_year);
+        $result = $CI->db->get()->row_array();
+
+        return $result['year_id'];
     }
 
 }
--- application/views/pricing_management/variety_list.php
+++ application/views/pricing_management/variety_list.php
@@ -10,13 +10,17 @@
             <th class="text-center"><?php echo $this->lang->line('LABEL_VARIETY')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_TARGETED_QUANTITY')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_TARGET_PROFIT_PER')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_AUTOMATED_MRP')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_LAST_YEAR_MRP')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_SALES_COMMISSION_PER')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_SALES_BONUS_MGT')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_OTHER_INCENTIVE_MGT')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_MRP_BY_MGT')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_NET_SALES_PRICE')?></th>
-            <th class="text-center"><?php echo $this->lang->line('LABEL_SALES_BONUS')?></th>
-            <th class="text-center"><?php echo $this->lang->line('LABEL_OTHER_INCENTIVE')?></th>
-            <th class="text-center"><?php echo $this->lang->line('LABEL_TOTAL_BUDGETED_MRP')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_NET_PROFIT')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_TOTAL_NET_SALES')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_TOTAL_NET_PROFIT')?></th>
+            <th class="text-center"><?php echo $this->lang->line('LABEL_PROFIT_PER')?></th>
             <th class="text-center"><?php echo $this->lang->line('LABEL_REMARKS')?></th>
 
             <?php
@@ -25,11 +29,26 @@
 
             foreach($varieties as $key=>$variety)
             {
-                $detail = Pricing_helper::get_pricing_automated_info($year, $variety['varriety_id']);
+                $net_sales_price = 0;
+                $total_net_sales_price = 0;
+                $net_profit = 0;
+                $total_net_profit = 0;
+                $profit_percentage = 0;
 
-                $pre_net_sales_price = ($detail['pi_value'] + ($detail['ho_and_gen_exp']/100)*$detail['pi_value'] + ($detail['marketing']/100)*$detail['pi_value'] + ($detail['finance_cost']/100)*$detail['pi_value']);
-                $net_sales_price = round($pre_net_sales_price + ($detail['target_profit']/100)*$pre_net_sales_price, 2);
-                $existing_data = Pricing_helper::get_pricing_automated_existing_info($year, $variety['varriety_id']);
+                $detail = Pricing_helper::get_pricing_management_info($year, $variety['varriety_id']);
+
+                $total_cogs = ($detail['pi_value'] + ($detail['ho_and_gen_exp']/100)*$detail['pi_value'] + ($detail['marketing']/100)*$detail['pi_value'] + ($detail['finance_cost']/100)*$detail['pi_value']);
+                $existing_data = Pricing_helper::get_pricing_initial_existing_info($year, $variety['varriety_id']);
+
+                if(is_array($existing_data) && sizeof($existing_data)>0)
+                {
+                    $net_sales_price = round($existing_data['mrp'] - ($existing_data['sales_bonus']/100)*$existing_data['mrp'] - ($existing_data['other_incentive']/100)*$existing_data['mrp'] - ($detail['sales_commission']/100)*$existing_data['mrp'], 2);
+                    $total_net_sales_price = round($net_sales_price*$detail['principal_quantity'], 2);
+
+                    $net_profit = round($existing_data['mrp'] - $total_cogs, 2);
+                    $total_net_profit = round($net_profit*$detail['principal_quantity'], 2);
+                    $profit_percentage = round(($total_net_profit/$total_cogs)*100, 2);
+                }
             ?>
             <tr class="main_tr">
                 <td class="text-center">
@@ -68,16 +87,20 @@
                     }
                     ?>
                 </td>
-                <td class="text-center"><?php echo $variety['varriety_name'];?></td>
-                <td class="text-center"><?php echo $detail['principal_quantity'];?></td>
+                <td class="text-center"><?php echo $variety['varriety_name'];?><input type="hidden" name="total_cogs" class="total_cogs" value="<?php echo $total_cogs;?>" /></td>
+                <td class="text-center"><?php echo $detail['principal_quantity'];?><input type="hidden" name="targeted_quantity" class="targeted_quantity" value="<?php echo $detail['principal_quantity'];?>" /></td>
                 <td class="text-center"><?php echo $detail['target_profit'];?></td>
-                <td class="text-center"><?php echo $detail['sales_commission'];?></td>
-                <td class="text-center"><?php echo $net_sales_price;?><input type="hidden" name="net_sales_price" class="net_sales_price" value="<?php echo $net_sales_price;?>" /></td>
+                <td class="text-center"><?php echo $detail['automated_mrp'];?></td>
+                <td class="text-center"><?php echo $detail['last_year_mrp'];?></td>
+                <td class="text-center"><?php echo $detail['sales_commission'];?><input type="hidden" name="sales_commission" class="sales_commission" value="<?php echo $detail['sales_commission'];?>" /></td>
                 <td class="text-center"><input type="text" name="pricing[<?php echo $variety['crop_id'];?>][<?php echo $variety['product_type_id'];?>][<?php echo $variety['varriety_id'];?>][sales_bonus]" class="form-control sales_bonus numbersOnly" value="<?php echo isset($existing_data['sales_bonus'])?$existing_data['sales_bonus']:'';?>" /></td>
                 <td class="text-center"><input type="text" name="pricing[<?php echo $variety['crop_id'];?>][<?php echo $variety['product_type_id'];?>][<?php echo $variety['varriety_id'];?>][other_incentive]" class="form-control other_incentive numbersOnly" value="<?php echo isset($existing_data['other_incentive'])?$existing_data['other_incentive']:'';?>" /></td>
-                <td class="text-center"><input type="text" readonly name="pricing[<?php echo $variety['crop_id'];?>][<?php echo $variety['product_type_id'];?>][<?php echo $variety['varriety_id'];?>][mrp]" class="form-control total_budgeted_mrp numbersOnly" value="<?php echo isset($existing_data['mrp'])?$existing_data['mrp']:'';?>" /></td>
-                <td class="text-center total_net_sales"><?php echo $net_sales_price*$detail['principal_quantity'];?></td>
-                <td class="text-center total_net_profit"><?php echo $detail['principal_quantity']*$detail['target_profit'];?></td>
+                <td class="text-center"><input type="text" name="pricing[<?php echo $variety['crop_id'];?>][<?php echo $variety['product_type_id'];?>][<?php echo $variety['varriety_id'];?>][mrp]" class="form-control mrp_by_mgt numbersOnly" value="<?php echo isset($existing_data['mrp'])?$existing_data['mrp']:'';?>" /></td>
+                <td class="text-center net_sales_price"><?php if(isset($net_sales_price)){echo $net_sales_price;}?></td>
+                <td class="text-center net_profit"><?php if(isset($total_net_sales_price)){echo $total_net_sales_price;}?></td>
+                <td class="text-center total_net_sales"><?php if(isset($net_profit)){echo $net_profit;}?></td>
+                <td class="text-center total_net_profit"><?php if(isset($total_net_profit)){echo $total_net_profit;}?></td>
+                <td class="text-center profit_percentage"><?php if(isset($profit_percentage)){echo $profit_percentage;}?></td>
                 <td class="text-center" style="vertical-align: middle;">
                     <label class="label label-primary load_remark">+R</label>
                     <div class="row popContainer" style="display: none;">
@@ -121,14 +144,29 @@
             $(".popContainer").hide();
         });
 
-        $(document).on("blur",".other_incentive",function()
+        $(document).on("keyup",".mrp_by_mgt",function()
         {
-            var net_sales_price = parseFloat($(this).closest('.main_tr').find(".net_sales_price").val());
+            var total_cogs = parseFloat($(this).closest('.main_tr').find(".total_cogs").val());
+            var targeted_quantity = parseInt($(this).closest('.main_tr').find(".targeted_quantity").val());
+            var sales_commission = parseFloat($(this).closest('.main_tr').find(".sales_commission").val());
             var sales_bonus = parseFloat($(this).closest('.main_tr').find(".sales_bonus").val());
-            var other_incentive = parseFloat($(this).val());
+            var other_incentive = parseFloat($(this).closest('.main_tr').find(".other_incentive").val());
+            var mrp_by_mgt = parseFloat($(this).val());
 
-            var total_budgeted_mrp = (net_sales_price + (sales_bonus/100)*net_sales_price + (other_incentive/100)*net_sales_price).toFixed(2);
-            $(this).closest('.main_tr').find(".total_budgeted_mrp").val(total_budgeted_mrp);
+            var net_sales_price = (mrp_by_mgt - (sales_commission/100)*mrp_by_mgt - (sales_bonus/100)*mrp_by_mgt - (other_incentive/100)*mrp_by_mgt).toFixed(2);
+            var total_net_sales_price = net_sales_price*targeted_quantity;
+
+            $(this).closest('.main_tr').find(".net_sales_price").html(net_sales_price);
+            $(this).closest('.main_tr').find(".total_net_sales").html(total_net_sales_price);
+
+            var net_profit = (total_cogs - mrp_by_mgt).toFixed(2);
+            var total_net_profit = (net_profit*targeted_quantity).toFixed(2);
+
+            $(this).closest('.main_tr').find(".net_profit").html(net_profit);
+            $(this).closest('.main_tr').find(".total_net_profit").html(total_net_profit);
+
+            var profit_percentage = ((total_net_profit/total_cogs)*100).toFixed(2);
+            $(this).closest('.main_tr').find(".profit_percentage").html(profit_percentage);
         });
     });
 </script>
